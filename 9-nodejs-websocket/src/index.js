85635e881d0b02aaa2a891f1f48dfad9051ae320
shdkej <shdkej@github.com>
2021-05-07 10:07:33 +0900

    feat: 🎸 add nodejs websocket

---
+++ 9-nodejs-websocket/src/index.js
@@ -0,0 +1,9 @@
+var WebSocketS = require("ws").Server;
+var wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function (ws) {
+    ws.send("Hello! I am a server.");
+    ws.on("message", function (message) {
+        console.log("Received: %s", message);
+    });
+});
